288684a1f28f824af831c0550e05714753877e13
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-03-23 14:05:26 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -31,3 +31,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+    def __str__(self):
+        """Returns a string representation of the BaseModel instance."""
+        return "{} {} {}".format(self.__class__.__name__, self.id, self.__dict__)
