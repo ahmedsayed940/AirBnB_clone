abfec901723ce6af1bdc82e0e6c5aa095d81c985
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-03-23 16:28:03 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Defines all common attributes/methods for other classes.
 """
-from models import storage
+import models
 import uuid
 from datetime import datetime
 
@@ -32,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.engine.file_storage.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the BaseModel instance."""
@@ -42,7 +42,7 @@ class BaseModel:
     def save(self):
         """Updates the current datetime and saves the instance."""
         self.updated_at = datetime.now()
-        storage.save()
+        models.engine.file_storage.storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of the BaseModel instance."""
