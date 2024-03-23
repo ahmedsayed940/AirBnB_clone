c1b3d9355ef13b7cf12d2ea698e0cf687be6b921
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-23 16:07:20 -0700

    Fix

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.engine.file_storage.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the BaseModel instance."""
@@ -41,6 +42,7 @@ class BaseModel:
     def save(self):
         """Updates the current datetime and saves the instance."""
         self.updated_at = datetime.now()
+        models.engine.file_storage.storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of the BaseModel instance."""
