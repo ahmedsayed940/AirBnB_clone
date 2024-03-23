952f55560ece348821a6a4421191025c5f05741a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-23 14:25:57 -0700

    Fixed the code

--- models/base_model.py
+++ models/base_model.py
@@ -29,8 +29,8 @@ class BaseModel:
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now
-            self.updated_at = datetime.now
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the BaseModel instance."""
