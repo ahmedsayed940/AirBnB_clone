a5ec73bc075802a72ba80ec2b22ff795bd8782a6
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-23 15:54:47 -0700

    Permissions

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Defines all common attributes/methods for other classes.
 """
+import models
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -32,7 +32,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the BaseModel instance."""
@@ -42,7 +42,7 @@ class BaseModel:
     def save(self):
         """Updates the current datetime and saves the instance."""
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of the BaseModel instance."""
