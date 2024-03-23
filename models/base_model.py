a4c47ec62cf6cbe251a28136fc4e72c76b9d475a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-23 15:59:40 -0700

    Cleaned

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the BaseModel instance."""
@@ -42,7 +41,6 @@ class BaseModel:
     def save(self):
         """Updates the current datetime and saves the instance."""
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of the BaseModel instance."""
