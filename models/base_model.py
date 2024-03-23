e1753861305ec1ac4dd8d7e41337ca597fd23a1d
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-23 13:52:50 -0700

    init

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Defines all common attributes/methods for other classes.
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base class for other classes, providing common attributes/methods.
+
+    Attributes:
+        id (str): Unique identifier for each instance.
+        created_at (datetime): Date and time when the instance was created.
+        updated_at (datetime): Date and time when the instance was updated.
+    """
+    def __init__(self, *args, **kwargs):
+        """Initializes a BaseModel instance.
+
+        Args:
+            args: Unused
+            kwargs: Key-value pairs of attributes
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at' or key == 'created_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
