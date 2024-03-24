bd8b19e42d07178d9b679188c8e5708cc1fd4571
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-24 14:49:20 -0700

    BaseModel test

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Module for testing BaseModel class.
+"""
+
+
+import unittest
+from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    """Class for testing BaseModel class.
+    """
+    def test_init(self):
+        """Tests init."""
+        my_model = BaseModel()
+
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
+
+    def test_save(self):
+        """Tests save."""
+        my_model = BaseModel()
+        updated_at_before = my_model.updated_at
+        updated_at_after = my_model.save()
+
+        self.assertNotEqual(updated_at_before, updated_at_after)
+
+    def test_to_dict(self):
+        """Tests to_dict."""
+        my_model = BaseModel()
+        Mdict = my_model.to_dict()
+
+        self.assertIsInstance(Mdict, dict)
+        self.assertEqual(Mdict["__class__"], 'BaseModel')
+        self.assertEqual(Mdict['id'], my_model.id)
+        self.assertEqual(Mdict['created_at'], my_model.created_at.isoformat())
+        self.assertEqual(Mdict['updated_at'], my_model.updated_at.isoformat())
+
+    def test_str(self):
+        """Tests str."""
+        my_model = BaseModel()
+
+        self.assertTrue(str(my_model).startswith('[BaseModel]'))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.__dict__), str(my_model))
+
+if __name__ == '__main__':
+    unittest.main()
