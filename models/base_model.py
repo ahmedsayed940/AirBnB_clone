1acfd27d2f372871045326c88280a0fa9f37ce97
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-24 13:35:43 -0700

    Fixed docs

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
+"""Defines all common attributes/methods for other classes.
 """
-Defines all common attributes/methods for other classes.
-"""
+
+
 import uuid
 from datetime import datetime
 import json
+import models
+
 
 class BaseModel:
+    """Defines all common attributes/methods for other classes.
     """
-    BaseModel class defines common attributes and methods for other classes.
 
-    Public instance attributes:
-    - id: string - Unique identifier assigned with a UUID when an instance is created.
-    - created_at: datetime - Date and time when an instance is created.
-    - updated_at: datetime - Date and time when an instance is last updated.
-
-    Public instance methods:
-    - __init__(self): Initializes a new instance of BaseModel with a unique id and creation/update timestamps.
-    - __str__(self): Returns a string representation of the BaseModel instance.
-    - save(self): Updates the 'updated_at' attribute with the current datetime.
-    - to_dict(self): Returns a dictionary representation of the BaseModel instance.
-
-    """
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instance of BaseModel with a unique id and creation/update timestamps.
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
         """
         if kwargs:
             for key, value in kwargs.items():
@@ -39,27 +33,18 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-        Returns a string representation of the BaseModel instance.
-
-        Returns:
-            str: String representation containing class name, id, and attribute dictionary.
+        """Returns a string representation of the BaseModel instance.
         """
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """
-        Updates the current datetime and saves the instance.
+        """Updates the current datetime and saves the instance.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-        Returns a dictionary representation of the BaseModel instance.
-
-        Returns:
-            dict: Dictionary containing all instance attributes with class name, creation/update times.
+        """Returns a dictionary representation of the BaseModel instance.
         """
         obj = self.__dict__.copy()
         obj['__class__'] = self.__class__.__name__
