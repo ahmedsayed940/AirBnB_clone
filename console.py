b3815cd53b8f1553d2d0f3e7ac6615d9f9992e56
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-24 18:10:46 -0700

    Show

--- console.py
+++ console.py
@@ -3,11 +3,14 @@
 """
 
 import cmd
+import shlex
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand."""
     prompt = "(hbnb)"
+    valid_classes = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -20,6 +23,48 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """An empty line + ENTER shouldn’t execute anything."""
         pass
+    
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it and prints the id."""
+        cmds = shlex.split(arg)
+
+        if len(cmds) == 0:
+            print ("** class name missing **")
+        elif cmds[0] not in self.valid_classes:
+            print("** class doesn't exist**")
+        else:
+            new_instance = BaseModel()
+            new_instance.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance
+        based on the class name and id."""
+        cmds = shlex.split(arg)
+
+        if len(cmds) == 0:
+            print ("** class name missing **")
+        elif cmds[0] not in self.valid_classes:
+            print("** class doesn't exist**")
+        elif len(cmds) == 1:
+            print("** instance id missing **")
+        else:
+            objs = storage.all()
+
+        key = "{}.{}".format(cmds[0], cmds[1])
+        if key not in objs:
+            print("** no instance found **")
+        else:
+            print(models.storage.all()[key])
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id."""
+    def do_all(self, arg):
+        """Prints all string representation of all instances
+        based or not on the class name."""
+    def do_update(self, arg):
+        """pdates an instance based on the class name and id
+        by adding or updating attribute."""
 
 
 if __name__ == '__main__':
