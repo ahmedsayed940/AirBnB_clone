4b986b21c4d4478c4d43df8831724aad7519265a
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-25 15:48:17 -0700

    Fixed

--- console.py
+++ console.py
@@ -77,22 +77,9 @@ class HBNBCommand(cmd.Cmd):
                 del objects[key]
                 storage.save()
 
-       def do_all(self, arg):
+    def do_all(self, arg):
         """Prints all string representation of all instances
         based or not on the class name."""
-        objs = storage.all
-        cmds = shlex.split(arg)
-
-        if len(cmds) == 0:
-            for key, value in ojs.items():
-                print(str(value))
-        elif cmds[0] not in self.valid_classes:
-            print("* class doesn't exist*")
-        else:
-            for key, value in objs.items():
-                if key.split('.')[0] == cmds[0]
-                print(str(value))
-
     def do_update(self, arg):
         """pdates an instance based on the class name and id
         by adding or updating attribute."""
