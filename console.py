fc2b81e8b900e736d400a3b1b02f1996f7cf0262
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-03-25 16:30:37 -0700

    fix1

--- console.py
+++ console.py
@@ -10,7 +10,7 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand."""
     prompt = "(hbnb)"
-    valid_classes = ["BaseModel"]
+    valid_classes = {"BaseModel"}
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
