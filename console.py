0bc24cc86439ee8dcc4454cb4ce84a0deb035b30
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-25 17:49:39 -0700

    Fixed typo

--- console.py
+++ console.py
@@ -11,7 +11,7 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand."""
     prompt = "(hbnb)"
-    valid_classes = ["BaseModel, User"]
+    valid_classes = ["BaseModel", "User"]
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
