b182ade51b098059d490c1c09023099ca4b9a4d4
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-26 14:49:03 -0700

    City

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Moudle of City inherit from BaseModel.
+"""
+
+from models import BaseModel
+
+
+class City(BaseModel):
+    """Class City that inherits from BaseModel."""
+    state_id = ""
+    name = ""
