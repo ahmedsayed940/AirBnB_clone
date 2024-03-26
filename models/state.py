73bd6c84482d731dab12ec86b7cf2736627d1052
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-26 14:46:47 -0700

    Created State

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Moudle of State inherit from BaseModel.
+"""
+
+from models import BaseModel
+
+
+class State(BaseModel):
+    """Class State that inherits from BaseModel."""
+    name = ""
