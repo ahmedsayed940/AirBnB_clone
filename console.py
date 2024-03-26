e36b22b5e442b0d2ebf620f4b1472fc18f7e34a8
AbdullahHR10 <AbdullahH.Ragab10@gmail.com>
2024-03-25 17:52:40 -0700

    Imported user

--- console.py
+++ console.py
@@ -5,6 +5,7 @@
 import cmd
 import shlex
 from models.base_model import BaseModel
+from models.user import User
 from models import storage
 
 
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,8 +35,8 @@ class FileStorage:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
                 for key, value in objdict.items():
-                    cls_name = val["class"]
-                    del val["class"]
+                    cls_name = val["__class__"]
+                    del val["__class__"]
                     obj_id = key.split('.')[1]
                     if cls_name == "BaseModel":
                         self.new(BaseModel(**val))
