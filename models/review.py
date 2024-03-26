5fd14206247677a16d2c0fcb322f1f3b403f1643
ahmedsayed940 <ahmeds.space940@gmail.com>
2024-03-26 15:06:52 -0700

    review

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" The Review class."""
+from models import BaseModel
+
+class Review(BaseModel):
+    """Represent a review.
+
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
